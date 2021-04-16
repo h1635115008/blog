2ff39d1794c4d90ab6b5dad77141e84a898e38d1
huangming <huangming@yuantutech.com>
2021-04-17 05:09:57 +0800

    增加响应状态码

--- blog-common/src/main/java/cn/aftertomorrow/common/enumeration/ResultCodeEnums.java
+++ blog-common/src/main/java/cn/aftertomorrow/common/enumeration/ResultCodeEnums.java
@@ -3,18 +3,34 @@ package cn.aftertomorrow.common.enumeration;
 /**
  * 响应码
  *
- * @author keriezhang
+ * @author huangming
  * @date 2021/04/13
  */
 public enum ResultCodeEnums {
     /**
      * 成功响应
      */
-    SUCCESS(0, "响应成功");
+    SUCCESS(0, "成功"),
+    /**
+     * 参数错误
+     */
+    ARGS_ERROR(-1, "参数错误"),
+    /**
+     * 服务端异常
+     */
+    SERVER_EXCEPTION(-2, "服务端异常"),
+    /**
+     * 业务异常
+     */
+    BIZ_ERROR(-3, "业务失败"),
+    /**
+     * 超时
+     */
+    SERVER_TIMEOUT(-4, "服务超时");
 
-    private Integer code;
+    private final Integer code;
 
-    private String desc;
+    private final String desc;
 
     ResultCodeEnums(Integer code, String desc) {
         this.code = code;
